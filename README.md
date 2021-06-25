91e3845599afa5ee1f8b71f7d715942ce6440d61
OnLee3 <dlwotjr2@gmail.com>
2021-06-25 20:26:50 +0900

    to chapter 4.2

---
+++ README.md
@@ -0,0 +1,15 @@
+# Wetube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit Profile
+/users/delete -> Delete Profile
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comment -> Comment on a Video
+/videos/comments/delete -> Delete a Comment a Video
--- src/server.js
+++ src/server.js
@@ -1,24 +1,20 @@
 import express from "express";
 import morgan from "morgan";
 
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res) => {
-    console.log("I will respond.");
-   return res.send("I love middlewares");
-}; 
-
-const login = (req, res) => {
-    return res.send("login")
-}
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`)
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
