724e37896a317f7eabd388d91f45731ff80298ad
OnLee3 <dlwotjr2@gmail.com>
2021-06-26 22:46:39 +0900

    to Chapter 4

---
+++ src/controllers/videoController.js
@@ -0,0 +1,16 @@
+export const trending = (req, res) => res.send("Home Page Videos");
+export const see = (req, res) => {
+    console.log(req.params);
+    return res.send(`Watch Video #${req.params.id}`);
+};
+export const edit = (req, res) => {
+    console.log(req.params);
+    return res.send("Edit")};
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => {
+    console.log(req.params);
+    return res.send("Upload")};
+export const deleteVideo = (req, res) => {
+    console.log(req.params);
+    return res.send("Delete Video")};
+
