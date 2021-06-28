d3ed5ba7c6a5ffd52c30fb65e46bc2c699feaf84
OnLee3 <dlwotjr2@gmail.com>
2021-06-28 23:32:42 +0900

    to chapter 5.2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,12 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) => {
+    res.render("home");
+};
 export const see = (req, res) => {
-    console.log(req.params);
-    return res.send(`Watch Video #${req.params.id}`);
+    res.render("watch");
 };
 export const edit = (req, res) => {
-    console.log(req.params);
-    return res.send("Edit")};
+    res.render("edit");
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => {
     console.log(req.params);
@@ -13,4 +14,3 @@ export const upload = (req, res) => {
 export const deleteVideo = (req, res) => {
     console.log(req.params);
     return res.send("Delete Video")};
-
