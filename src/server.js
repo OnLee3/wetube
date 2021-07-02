f20b019b57d3e438638e8c5218df3ee647278ac8
OnLee3 <dlwotjr2@gmail.com>
2021-07-03 00:17:57 +0900

    to Chapter 6.8

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
+import "./db";
 import express from "express";
 import morgan from "morgan";
-
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -15,6 +15,7 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
+app.use(express.urlencoded({extended:true}));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
