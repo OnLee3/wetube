d9432e91ae5f36daddd474fcb42a54ab4782fa3a
OnLee3 <dlwotjr2@gmail.com>
2021-07-15 22:04:19 +0900

    to chapter 8.3

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -30,7 +30,6 @@ export const postJoin = async(req, res) => {
     });
 }
 };
-export const edit = (req, res) => res.send("Edit User");
 export const getLogin = (req, res) => res.render("login", {pageTitle:"Login"});
 export const postLogin = async(req, res) => {
     const {username, password} = req.body;
@@ -39,16 +38,16 @@ export const postLogin = async(req, res) => {
     if(!user){
         return res.status(400).render("login",
         {
-        pageTitle,
-         errorMessage:"An account with this username does not exists. "
+            pageTitle,
+            errorMessage:"An account with this username does not exists. "
         })
     }
     const ok = await bcrypt.compare(password, user.password);
     if(!ok){
         return res.status(400).render("login",
         {
-        pageTitle,
-         errorMessage:"Wrong password!"
+            pageTitle,
+            errorMessage:"Wrong password!"
         })
     }
     req.session.loggedIn = true;
@@ -78,58 +77,118 @@ export const finishGithubLogin = async(req,res) => {
     }
     const params = new URLSearchParams(config).toString();
     const finalUrl = `${baseUrl}?${params}`
-
+    
     const tokenRequest = await (
         await fetch(finalUrl, {
-        method:"POST",
-        headers:{
-            Accept: "application/json",
-        },
-    })).json();
-    if("access_token" in tokenRequest){
-        const {access_token} = tokenRequest;
-        const apiUrl = "https://api.github.com";
-        const userData = await (await fetch(`${apiUrl}/user`, {
+            method:"POST",
             headers:{
-                Authorization: `token ${access_token}`
-            }
+                Accept: "application/json",
+            },
         })).json();
-        console.log(userData);
-        const emailData = await (
-            await fetch(`${apiUrl}/user/emails`, {
+        if("access_token" in tokenRequest){
+            const {access_token} = tokenRequest;
+            const apiUrl = "https://api.github.com";
+            const userData = await (await fetch(`${apiUrl}/user`, {
                 headers:{
                     Authorization: `token ${access_token}`
                 }
-            })
-         ).json();
-         console.log(emailData);
-         const emailObj = emailData.find(email => email.primary === true && email.verified ===  true
-            );
-            if(!emailObj){
-                return res.redirect("/login");
-            }
-            let user = await User.findOne({email: emailObj.email});
-            if(!user){
-                    user = await User.create({
-                    avatarUrl: userData.avatar_url,
-                    name: userData.name,
-                    username: userData.login,
-                    email: emailObj.email,
-                    password:"",
-                    socialOnly:true,
-                    location: userData.location,
+            })).json();
+            console.log(userData);
+            const emailData = await (
+                await fetch(`${apiUrl}/user/emails`, {
+                    headers:{
+                        Authorization: `token ${access_token}`
+                    }
                 })
-            } 
-                req.session.loggedIn = true;
-                req.session.user = user;
+                ).json();
+                console.log(emailData);
+                const emailObj = emailData.find(email => email.primary === true && email.verified ===  true
+                    );
+                    if(!emailObj){
+                        return res.redirect("/login");
+                    }
+                    let user = await User.findOne({email: emailObj.email});
+                    if(!user){
+                        user = await User.create({
+                            avatarUrl: userData.avatar_url,
+                            name: userData.name,
+                            username: userData.login,
+                            email: emailObj.email,
+                            password:"",
+                            socialOnly:true,
+                            location: userData.location,
+                        })
+                    } 
+                    req.session.loggedIn = true;
+                    req.session.user = user;
+                    return res.redirect("/");
+                } else {
+                    return res.redirect("/login");
+                }
+            }
+            
+            export const logout = (req, res) => {
+                req.session.destroy();
                 return res.redirect("/");
-    } else {
-        return res.redirect("/login");
-    }
-}
+            }
+            export const getEdit = (req, res) => {
+                return res.render("edit-profile", {pageTitle:"Edit Profile"});
+            }
+            export const postEdit = async(req, res) => {
+                const {
+                    session: {
+                        user : {
+                            _id,
+                            name : sessionName,
+                            email : sessionEmail,
+                            username : sessionUserName,
+                            location : sessionLocation,
+                                },
+                            },
 
-export const logout = (req, res) => {
-    req.session.destroy();
-    return res.redirect("/");
-}
-export const see = (req, res) => res.send("See User"); 
+                        body : {name, email, username, location}
+                    } = req;
+            //email validation
+            if (email !== sessionEmail){
+                const emailExists = await User.exists({email});
+                if(emailExists){
+                    return res.status(400).render("edit-profile", {pageTitle:"Edit Profile", errorMessage:"This email is already taken."})
+                };
+                const updatedUser = await User.findByIdAndUpdate(_id, {
+                    name:name, 
+                    email:email, 
+                    username:username, 
+                    location:location,
+                }, {new: true})
+                req.session.user = updatedUser;
+                return res.redirect("/users/edit");
+            }   
+            //username validation.
+                else if(username !== sessionUserName){
+                const userNameExists = await User.exists({username});
+                if(userNameExists){
+                    return res.status(400).render("edit-profile", {pageTitle:"Edit Profile", errorMessage:"This username is already taken."})
+                }
+                const updatedUser = await User.findByIdAndUpdate(_id, {
+                    name:name, 
+                    email:email, 
+                    username:username, 
+                    location:location,
+                }, {new: true})
+                req.session.user = updatedUser;
+                return res.redirect("/users/edit");
+            }
+            //name, location check. 중복되도 상관 없음    
+            else if( name !== sessionName ||
+                location !== sessionLocation){
+                const updatedUser = await User.findByIdAndUpdate(_id, {
+                    name:name, 
+                    location:location,
+                }, {new: true})
+                req.session.user = updatedUser;
+                return res.redirect("/users/edit");
+            } else {
+                return res.redirect("/");
+            }
+        }
+            export const see = (req, res) => res.send("See User"); 
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { getJoin, getLogin,postLogin, postJoin } from "../controllers/userController";
 import { home, search} from "../controllers/videoController";
+import { publicOnlyMiddleware } from "../middlewares";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.route("/Login").get(getLogin).post(postLogin);
+rootRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
+rootRouter.route("/Login").all(publicOnlyMiddleware).get(getLogin).post(postLogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;
