2ee0e5c095999a46bc3b750891bdb637e6f10c49
OnLee3 <dlwotjr2@gmail.com>
2021-07-20 22:28:20 +0900

    to 8.14

--- src/models/User.js
+++ src/models/User.js
@@ -13,9 +13,9 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre('save', async function() {
-    console.log("Users password:", this.password);
+    if(this.isModified("password")){
     this.password = await bcrypt.hash(this.password, 5);
-    console.log("Hash Password:", this.password);
+    }
 })
 
 const User = mongoose.model('User', userSchema);
