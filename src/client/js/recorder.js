4ae64d6d65c5c804ab4a532dffa528701a9ceac4
OnLee3 <dlwotjr2@gmail.com>
2021-08-07 11:10:09 +0900

    13.4

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -3,9 +3,15 @@ const video = document.getElementById("preview");
 
 let stream;
 let recorder;
+let videoFile;
 
 const handleDownload = () => {
-
+    const a = document.createElement("a");
+    a.href = videoFile;
+    // URL로 가는 대신, 다운로드하게 만들어줌
+    a.download = "MyRecording.webm"
+    document.body.appendChild(a);
+    a.click();
 }
 
 const handleStop = () => {
@@ -24,7 +30,7 @@ const handleStart = () => {
     recorder = new MediaRecorder(stream);
     recorder.ondataavailable = (event) => {
         // 브라우저 메모리에서만 가능한 URL을 만들어줌
-        const videoFile = URL.createObjectURL(event.data)
+        videoFile = URL.createObjectURL(event.data)
         video.srcObject = null;
         video.src = videoFile;
         video.loop = true;
