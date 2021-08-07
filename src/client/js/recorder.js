037bbc8a25d26c0e80bbb552bea364f121377a08
OnLee3 <dlwotjr2@gmail.com>
2021-08-07 10:40:55 +0900

    13.3

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -2,11 +2,18 @@ const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
 let stream;
+let recorder;
+
+const handleDownload = () => {
+
+}
 
 const handleStop = () => {
-    startBtn.innerText = "Start Recording";
+    startBtn.innerText = "Download Recording";
     startBtn.removeEventListener("click", handleStop);
-    startBtn.addEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleDownload);
+    recorder.stop();
+
 }
 
 const handleStart = () => {
@@ -14,16 +21,16 @@ const handleStart = () => {
     startBtn.removeEventListener("click", handleStart);
     startBtn.addEventListener("click", handleStop);
 
-    const recorder = new MediaRecorder(stream);
-    recorder.ondataavailable = (e) => {
-        console.log("recording done");
-        console.log(e);
-        console.log(e.data);
+    recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (event) => {
+        // 브라우저 메모리에서만 가능한 URL을 만들어줌
+        const videoFile = URL.createObjectURL(event.data)
+        video.srcObject = null;
+        video.src = videoFile;
+        video.loop = true;
+        video.play();
     }
-    console.log(recorder);
     recorder.start();
-    console.log(recorder);
-    setTimeout(() => {recorder.stop();}, 10000)
 }
 
 const init = async() => {
@@ -37,5 +44,4 @@ const init = async() => {
 }
 
 init();
-
 startBtn.addEventListener("click", handleStart);
