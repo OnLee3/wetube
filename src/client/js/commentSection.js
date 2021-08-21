1d8ec1976f30386586e933c3a4432d201d907765
OnLee3 <dlwotjr2@gmail.com>
2021-08-21 22:57:59 +0900

    16.7

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,6 +1,20 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+const addComment = (text) => {
+    const videoComments = document.querySelector(".video__comments ul");
+    const newComment = document.createElement("li");
+    newComment.className = "video__comment";
+    const icon = document.createElement("i");
+    icon.className = "fas fa-comment";
+    const span = document.createElement("span");
+    span.innerText = `  ${text}`
+    newComment.appendChild(icon);
+    newComment.appendChild(span);
+    // prepend() : element를 맨 위에 추가
+    videoComments.prepend(newComment);
+}
+
 
 const handleSubmit = async (e) => {
     e.preventDefault();
@@ -9,7 +23,7 @@ const handleSubmit = async (e) => {
     const videoId = videoContainer.dataset.id;
     // fetch : URL 변경없이, JS로 request를 보낼 수 있게함
     if(text === "") return;
-    await fetch(`/api/videos/${videoId}/comment`, {
+    const { status } = await fetch(`/api/videos/${videoId}/comment`, {
         method:"POST",
         headers : {
             "Content-Type" : "application/json",
@@ -18,7 +32,9 @@ const handleSubmit = async (e) => {
         body : JSON.stringify({ text }),
     });
     textarea.value = "";
-    window.location.reload();
+    if(status === 201){
+        addComment(text);
+    }
 }
 
 if(form) form.addEventListener("submit", handleSubmit);
