4c951231264672aea5c3fbc30e0db9b66de6ed7f
OnLee3 <dlwotjr2@gmail.com>
2021-08-24 15:55:31 +0900

    cross-origin

--- src/server.js
+++ src/server.js
@@ -36,6 +36,7 @@ app.use("/assets", express.static("assets"))
 app.use((req, res, next) => {
     res.header("Cross-Origin-Embedder-Policy", "require-corp");
     res.header("Cross-Origin-Opener-Policy", "same-origin");
+    res.header("Cross-Origin-Resource-Policy", "cross-origin");
     next();
     });
 app.use("/", rootRouter);
